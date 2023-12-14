760dffa58272460c973e5fe619fefc5f701e6556
Kelsey Barham <kelseylbarham@gmail.com>
2023-12-14 08:06:49 -0500

    created a basic form for the make a card page showing font, color, background, etc.

---
+++ social-cards/src/components/createCard.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+
+const CreateCard = ({token}) => {
+//     const [cards, setCards] = useState([])
+//     const [cardIndex, setCardIndex] = useState(0)
+//     const [loading, setLoading] = useState(true)
+
+// useEffect(() => {
+//     axios
+//         .get('', {
+//             headers: {
+//                 Authorization: `Token ${token}`,
+//             },
+//         })
+//         .then((res) => {
+            
+
+//         })
+// })
+
+    return (
+        <div>
+            <div className="makeACard">
+            <div className="sidebarOptions">
+            <div>
+                <span>Front Text</span>
+                <input type="text"/>
+            </div>
+            <div>
+                <span>Back Text</span>
+                <input type="text"/>
+            </div>
+            <div>
+                <span>Upload an image</span>
+                <input type="text"/>
+            </div>
+                <div>
+                    <label htmlFor="color">Select a background color</label>
+                    <input name="color" type="color" />
+                </div>
+                <div>
+                    <label htmlFor="font">Choose a font</label>
+                    <select name="font">
+                        <option value="Ariel">Ariel</option>
+                        <option value="Helvetica">Helvetica</option>
+                        <option value="Times New Roman">Times New Roman</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="previewCard">
+
+            </div>
+            </div>
+        </div>
+    )
+
+ 
+
+}
+
+export default CreateCard
