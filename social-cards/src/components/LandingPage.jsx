b7667c94f9555342f786dbf827a04459121befc0
Jordan Manus <jlmanus17@gmail.com>
2023-12-19 17:40:36 -0500

    added a new create card button to the landing page. added css to new button. need to correct link to the '/newCard' page (need to pass a token through the link.

--- social-cards/src/components/LandingPage.jsx
+++ social-cards/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import PreviewCard from './PreviewCard';
 import FollowUser from './FollowUser';
@@ -7,6 +8,7 @@ import Sidebar from './Sidebar'
 const LandingPage = ({ isAuthenticated, token }) => {
 
     const [cards, setCards] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
@@ -20,6 +22,14 @@ const LandingPage = ({ isAuthenticated, token }) => {
     return (
         <>
             <div className='flex flex-wrap flex-col md:flex-row gap-4'>
+                <div className="border my-4">
+                    <Link to={{ pathname: '/newCard' }} >
+                        <svg className='flex new-card ' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48" >
+                            <path fill="#4caf50" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"></path><path fill="#fff" d="M21,14h6v20h-6V14z"></path><path fill="#fff" d="M14,21h20v6H14V21z"></path>
+                        </svg>
+                        <h4>Create New Card</h4>
+                    </Link>
+                </div>
                 {cards.map((card) => {
                     return (
                         <>
@@ -44,7 +54,7 @@ const LandingPage = ({ isAuthenticated, token }) => {
                         </>
                     );
                 })}
-            </div>
+            </div >
         </>
     )
 }
