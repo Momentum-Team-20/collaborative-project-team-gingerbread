2db3292d68112b5f164c615240d18db819d36ac8
Jordan Manus <94319835+jordan-manus@users.noreply.github.com>
2023-12-20 17:30:36 -0500

    added back import statements and variables after merge.

--- social-cards/src/App.jsx
+++ social-cards/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import useLocalStorageState from 'use-local-storage-state';
+import Login from './components/Login';
+import LandingPage from './components/LandingPage'
+import Registration from './components/Registration';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import CreateCard from './components/createCard';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const [token, setToken] = useLocalStorageState("Token", "");
+  const isAuthenticated = token !== "";
+  const [cards, setCards] = useState([]);
+
+  const setAuth = (username, token) => {
+    setUsername(username);
+    setToken(token);
+    console.log(token);
+  }
 
   return (
     <>
       <Navbar isAuthenticated={isAuthenticated} setToken={setToken} token={token} username={username} />
-      {/* <Navbar token={token} setToken={setToken} /> */}
       <Routes>
         <Route
           path='/'
@@ -32,18 +48,7 @@ function App() {
         <Route
           path='/editCard/:card_id'
           element={<CreateCard token={token} username={username} />} />
-        {/* <Route
-          path='/logout'
-          element={
-            <PrivateRoute
-              setToken={setToken}
-              setUsername={setUsername}
-              username={username}
-            >
-              <Logout setUsername={setUsername} setToken={setToken} />
-            </PrivateRoute>
-          } */}
-        {/* ></Route> */}
+
       </Routes >
     </>
   )
